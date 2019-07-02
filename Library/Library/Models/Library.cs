31364e4305be9e37bdc283289664f6294e66d252
Wayne881004 <t0919247347@gmail.com>
2019-07-02 18:05:39 +0800

    2017/07/02

---
+++ Library/Library/Models/Library.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Library
+    {
+        /// <summary>
+        /// 書名
+        /// </summary>
+        [DisplayName("書名")]
+        public string  BookName { get; set; }
+
+        /// <summary>
+        /// 圖書類別
+        /// </summary>
+        [DisplayName("圖書類別")]
+        public int EmployeeClass { get; set; }
+
+        /// <summary>
+        /// 借閱人
+        /// </summary>
+        [DisplayName("借閱人")]
+        public int BookUser { get; set; }
+
+        /// <summary>
+        /// 借閱狀態
+        /// </summary>
+        [DisplayName("借閱狀態")]
+        public int BookStatus { get; set; }
+    }
+}
